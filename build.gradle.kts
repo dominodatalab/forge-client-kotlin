6efb9fdd226f3db2286a683ec635197bd966943e
Sonny Garcia <sonnygarcia@icloud.com>
2020-06-07 10:20:23 -0600

    fixes gradle settings
    adds gradlew script
    updates gradle settings so that tests are run correctly

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,11 @@
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
 plugins {
     kotlin("jvm") version "1.3.72"
 }
 
-group = "org.example"
+group = "com.dominodatalab"
 version = "1.0-SNAPSHOT"
 
 repositories {
@@ -16,6 +19,8 @@ dependencies {
     testImplementation(kotlin("test"))
     testImplementation(kotlin("test-junit5"))
     testImplementation("io.mockk:mockk:1.10.0")
+
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.2")
 }
 
 tasks {
@@ -24,5 +29,17 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+    test {
+        useJUnitPlatform()
+
+        testLogging {
+            showCauses = true
+            showExceptions = true
+            showStackTraces = true
+            exceptionFormat = TestExceptionFormat.FULL
+
+            events(TestLogEvent.PASSED, TestLogEvent.FAILED, TestLogEvent.SKIPPED)
+        }
     }
 }
